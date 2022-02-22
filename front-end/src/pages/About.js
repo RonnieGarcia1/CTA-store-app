287562fbca5aab1f0f334d131c63e562f9c70bde
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2022-02-21 22:44:08 -0500

    added about page

--- front-end/src/pages/About.js
+++ front-end/src/pages/About.js
@@ -2,9 +2,34 @@ import React from "react";
 
 const About = () => {
     return(
-        <div className="About-container">
-            <h4>Hike & Bike was founded in 2022 by a developer in New York City by the Name of Ronnie Garcia</h4>
-
+        <div className="about-header">
+                <h4>Hike & Bike was founded and created in 2022 by a developer in New York City as a final project</h4>
+                <p>Hike and Bike mission statement is “we inspire, educate and outfit for a lifetime of outdoor adventure and stewardship.
+                    ” This statement is a striking reflection of the commitment of Hike and Bike in the protection of the environment while
+                     at the same time delighting in its marvels. The statement has the following components:
+                <br></br>
+                <br></br>
+                <li>1.Improving lives. The primary purpose of Hike and Bike (Recreational Equipment) is to ensure that everyone gets a taste of how it feels to live 
+                life well by enjoying their time outdoors. In fact, the company demonstrates its conviction that people get the best out of themselves
+                by conquering the natural and untamed places in the way it awakens the love for such environments in its target customers. It does this 
+                through numerous approaches.
+                </li>
+                <br></br> 
+                <li>
+                2.Distinguished products. This component is a revelation of how Hike and Bike enables people to enhance their life, 
+                discover themselves, and gain fulfilment outdoor – use of top-quality camping gear, attires for sports, and other equipment. Clients have a wide range of 
+                choices to pick from, and there are categories for everyone and all occasions. 
+                </li>
+                <br></br>
+                <li>
+                3.Exceeding expectations. Hike and Bike has come of age considering 
+                that it offers much more than expected. Today, the company is one of the leading providers of courses in outdoors activities, consultations
+                in hiking and vacations and many others. The firm has learned the art of reaching out to its customers through its inspirational stories of the best outdoor experiences as well.
+                </li></p>
+                <br></br>
+                <br></br>
+                <br></br>
+                <img alt="family" src="https://montemlife.com/wp-content/uploads/2019/03/family.jpg" />
         </div>
     )
 }
