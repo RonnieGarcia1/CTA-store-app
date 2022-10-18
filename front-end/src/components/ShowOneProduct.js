41065e4a59e369ccc767b9ba861d9be64756feea
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2022-10-18 00:49:04 -0400

    added ShowOne.css file

--- front-end/src/components/ShowOneProduct.js
+++ front-end/src/components/ShowOneProduct.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import '../Styles/ShowOne.css';
 import { useEffect, useState } from "react";
 import { useParams, useNavigate, Link } from "react-router-dom";
 
